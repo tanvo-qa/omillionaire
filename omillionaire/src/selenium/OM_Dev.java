a9e38b17c793962bfcff8a172875eaf5b638db34
tanhungvo <tanhungvo@localhost>
2024-06-11 17:12:10 +0700

    Update recaptcha

--- omillionaire/src/selenium/OM_Dev.java
+++ omillionaire/src/selenium/OM_Dev.java
@@ -2,16 +2,21 @@ package selenium;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.KeyDownAction;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 
 
 
@@ -22,97 +27,129 @@ public class OM_Dev {
 	
 	String driverPath = "/Users/tanhungvo/Documents/SeleniumWebDrider/chromedriver-mac-arm64/chromedriver";
 	
-	String username = "super";
-
-
-	String password = "secret";
-
-
-	String domain = "dev-omillionaire.bitville.dev";
-	
 	
 	@BeforeClass
-	public void beforeClass() {
+	
+	public void beforeClass() throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
-
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments(new String[] {"--incognito"});
+		
+		driver = new ChromeDriver(chromeOptions);
+		
+		chromeOptions.addArguments("user-data-dir=/Users/tanhungvo/Library/Application Support/Google/Chrome/Profile 6");
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-		String url = "https://" + username + ":" + password + "@" + domain;
+		
+		String url = "https://dev-omillionaire.bitville.dev/";
 		
 		driver.get(url);
-		
+				
 	}
 	
-	public void openSignInPage() throws InterruptedException {
+	/*public void openSignUpPage() throws InterruptedException {
 		
-		driver.findElement(By.xpath("//div[@class='section-header__buttons__login' and text()= ' SIGN IN ']")).click();
+		driver.findElement(By.xpath("//div[@class='section-header__buttons__register' and text()= ' register ']")).click();
 		
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		
 		String url = driver.getCurrentUrl();
 		
-		Assert.assertEquals(url, "https://super:secret@dev-omillionaire.bitville.dev/login");
+		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/register");
 		
+	}
+	
+	public void signUpByMobile() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//input[@id='input-full-name']")).sendKeys("Nang No");
+		driver.findElement(By.xpath("//input[@id='register-phone']")).sendKeys("909566566");
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Demo@123");
+		
+		driver.findElement(By.xpath("//input[@id='checkbox']")).click();
+		
+		driver.findElement(By.xpath("//button[@type='button' and @class='btn btn-button primary btn-secondary']")).click();
+		
+		Thread.sleep(5000);
+		
+	}*/
+	
+	
+	public void openSignInPage() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//div[@class='section-header__buttons__login' and text()= ' LOGIN ']")).click();
+				
+		String url = driver.getCurrentUrl();
+		
+		driver.findElement(By.xpath("//button[@class='btn btn-button primary btn-secondary' and text() =' accept all ']")).click();
+		//button[@class='btn btn-button primary btn-secondary' and text() =' accept all ']
+						
+		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/login");
+				
 	}
 	
 	public void signInByMobile() throws InterruptedException {
 		
 		driver.findElement(By.xpath("//input[@id='input-phone-login']")).sendKeys("938799559");
+		Thread.sleep(3000);
+		
 		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Demo@123");
+		Thread.sleep(6000);
 		
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
-		Thread.sleep(6000);
-		
-		driver.findElement(By.xpath("//div[@class='user-section__info__dropdown']")).click();
-		
-		WebElement logOut = driver.findElement(By.xpath("//div[text()=' Logout ']"));
-		
-		String text = logOut.getText();
-		
-		Assert.assertEquals(text, "Logout");
+		Thread.sleep(7000);
+				
+		//Assert.assertEquals(driver.findElement(By.xpath("//div[@class='toast__content__title errorToast']")).getText(), "robot verification failed, please try again.");
 		
 	}
 	
-//	public void openBuyNowPage() {
-//		
-//		driver.findElement(By.xpath("//div[text()='  Buy now  ']")).click();
-//		
-//		String url = driver.getCurrentUrl();
-//		
-//		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/buy/green-certificates");
-//		
-//	}
-//	
-//	
-//	public void openNumberPage() {
-//		
-//		driver.findElement(By.xpath("//input[@id = '__BVID__135']")).sendKeys("100");
-//		driver.findElement(By.xpath("//div[@class='buy-now__action-btn btn-dt']//button[text()=' Buy now ']")).click();
-//		
-//		String url = driver.getCurrentUrl();
-//		
-//		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/numbers");
-//		
-//	}
-//	
-//	public void openCheckOutPage() {
-//		
-//		driver.findElement(By.xpath("//button[text()=' Quick Pick All Entries ']")).click();
-//		driver.findElement(By.xpath("//button[text()=' Add to Cart ']")).click();
-//		
-//		String url = driver.getCurrentUrl();
-//		
-//		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/checkout");
-//		
-//	}
+	public void updateProfile() throws InterruptedException {
 	
-	@AfterClass
+		driver.get("https://dev-omillionaire.bitville.dev/personal-detail");
+		
+		//Thread.sleep(3000);
+		
+		
+		//driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("recaptcha01@mailinator.com");
+		
+		driver.findElement(By.xpath("//input[@id='input-phone-personal']")).clear();
+		
+		driver.findElement(By.xpath("//input[@id='input-phone-personal']")).sendKeys("939557557");
+		
+		//input[@id='input-phone-personal']
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Demo@123");
+		
+		driver.findElement(By.xpath("//input[@id='input-confirmPassword']")).sendKeys("Demo@123");
+	
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		Thread.sleep(10000);
+	}
+	
+	/*public void forgotPassword() throws InterruptedException {
+		
+		driver.get("https://dev-omillionaire.bitville.dev/forgot-password");
+		
+		driver.findElement(By.xpath("//input[@id='input-phone-login']")).sendKeys("935455455");
+		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+	}*/
+	
+	
+	/*@AfterClass
 	public void afterClass() {
 		driver.quit();
-	}
+	}*/
 
 }
--- omillionaire/src/selenium/OM_Prod.java
+++ omillionaire/src/selenium/OM_Prod.java
@@ -1,48 +1,88 @@
 package selenium;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-
-
 @Test
 public class OM_Prod {
 	
 	public WebDriver driver;
+	
 	String driverPath = "/Users/tanhungvo/Documents/SeleniumWebDrider/chromedriver-mac-arm64/chromedriver";
-
+	
 	
 	@BeforeClass
 	public void beforeClass() {
 
 		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments(new String[] {"--incognito"});
+		
+		driver = new ChromeDriver(chromeOptions);
+		
 
-		driver.get("https://omillionaire.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		String url = "https://omillionaire.com/";
 		
-		driver.findElement(By.xpath("//a[@class='smt-close-icon smt-edit-close-icon NC_NOT_ACTIVE smt-close' and text()='X']")).click();
+		driver.get(url);
+		
 	}
 	
-	public void openSignInPage() throws InterruptedException {
+	public void openSignUpPage() throws InterruptedException {
 		
-		driver.findElement(By.xpath("//div[@class='section-header__buttons__login' and text()= ' SIGN IN ']")).click();
+		driver.findElement(By.xpath("//div[@class='section-header__buttons__register' and text()= ' register ']")).click();
 		
-		Thread.sleep(6000);
+		Thread.sleep(2000);
 		
 		String url = driver.getCurrentUrl();
 		
-		Assert.assertEquals(url, "https://omillionaire.com/login");
+		Assert.assertEquals(url, "https://omillionaire.com/register");
+		
+	}
+	
+	public void signUpByMobile() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//input[@id='input-full-name']")).sendKeys("Nang No");
+		driver.findElement(By.xpath("//input[@id='register-phone']")).sendKeys("909566566");
+		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Demo@123");
+		
+		driver.findElement(By.xpath("//input[@id='checkbox']")).click();
+		
+		driver.findElement(By.xpath("//button[@type='button' and text()=' register ']")).click();
+		
+		Thread.sleep(5000);
+		
+		//String text = driver.findElement(By.xpath("//p[text()=' OTP Verification ']")).getText();
+		
+		//Assert.assertEquals(text, "OTP VERIFICATION");
+		
+	}
+	
+	
+	/*public void openSignInPage() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//div[@class='section-header__buttons__login' and text()= ' LOGIN ']")).click();
+		
+		Thread.sleep(3000);
+		
+		String url = driver.getCurrentUrl();
+				
+		Assert.assertEquals(url, "https://omillionaire.com/login");		
 		
 	}
 	
@@ -50,28 +90,20 @@ public class OM_Prod {
 		
 		driver.findElement(By.xpath("//input[@id='input-phone-login']")).sendKeys("938799559");
 		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Demo@123");
-		
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
+				
+		Thread.sleep(3000);
 		
-		Thread.sleep(6000);
 		
-		driver.findElement(By.xpath("//div[@class='user-section__info__dropdown']")).click();
-		
-		WebElement logOut = driver.findElement(By.xpath("//div[text()=' Logout ']"));
-		
-		String text = logOut.getText();
-		
-		Assert.assertEquals(text, "Logout");
-		
-	}
+	}*/
 	
-	public void openBuyNowPage() {
+	/*public void openBuyNowPage() {
 		
 		driver.findElement(By.xpath("//div[text()='  Buy now  ']")).click();
 		
 		String url = driver.getCurrentUrl();
 		
-		Assert.assertEquals(url, "https://omillionaire.com/buy/green-certificates");
+		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/buy/green-certificates");
 		
 	}
 	
@@ -83,7 +115,7 @@ public class OM_Prod {
 		
 		String url = driver.getCurrentUrl();
 		
-		Assert.assertEquals(url, "https://omillionaire.com/numbers");
+		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/numbers");
 		
 	}
 	
@@ -94,13 +126,23 @@ public class OM_Prod {
 		
 		String url = driver.getCurrentUrl();
 		
-		Assert.assertEquals(url, "https://omillionaire.com/checkout");
+		Assert.assertEquals(url, "https://dev-omillionaire.bitville.dev/checkout");
 		
-	}
+	}*/
 	
-	@AfterClass
+	/*public void forgotPassword() throws InterruptedException {
+	
+	driver.get("https://omillionaire.com/forgot-password");
+	
+	driver.findElement(By.xpath("//input[@id='input-phone-login']")).sendKeys("935455455");
+	
+	driver.findElement(By.xpath("//button[@type='submit']")).click();
+	
+	}*/
+	
+	/*@AfterClass
 	public void afterClass() {
 		driver.quit();
-	}
+	}*/
 
 }
